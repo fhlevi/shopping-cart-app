8d13e24e47c1cffd3122ac02805f63f909ac33f8
Faisal Fahlevi <fhlevi2804@gmail.com>
2025-07-23 12:29:31 +0700

    fix: add console log for show product list data

--- packages/product-app/src/config/http.js
+++ packages/product-app/src/config/http.js
@@ -3,14 +3,14 @@ import Service from './service';
 
 export const HTTPClientNonAuth = () => {
     console.log("🔄 initializing HTTP client for non-authenticated requests");
-    
+
     const client = axios.create({
         baseURL: Service.API,
         headers: {
             'Accept': "application/json",
             'Content-Type': 'application/json'
         },
-        timeout: 120000,
+        timeout: 5000,
     })
 
     return client
